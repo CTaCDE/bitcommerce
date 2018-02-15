83104547c2a51c1f95cdc83ea9878b727c54b06b
Chris Ta <CTaCDE@users.noreply.github.com>
2018-02-15 15:30:35 -0800

    Add functionality to retrive items from database

--- app.js
+++ app.js
@@ -13,26 +13,75 @@
 
 'use strict';
 
+const mongodb = require('mongodb');
 const express = require('express');
 const app = express();
+const nconf = require('nconf');
 var path = require('path');
 
-// Load the static html/css/js files
-app.get('/', function(req, res){
-    res.sendFile('index.html', { root: __dirname + "/public/html" } );
-});
-app.use(express.static("public"));
+// Read in keys and secrets. Using nconf use can set secrets via
+// environment variables, command-line arguments, or a keys.json file.
+nconf.argv().env().file('keys.json');
 
+// Connect to a MongoDB server provisioned over at
+// MongoLab. See the README for more info.
+const user = nconf.get('mongoUser');
+const pass = nconf.get('mongoPass');
+const host = nconf.get('mongoHost');
+const port = nconf.get('mongoPort');
 
-if (module === require.main) {
-  // [START server]
-  // Start the server
-  const server = app.listen(process.env.PORT || 8080, () => {
-    const port = server.address().port;
-    console.log(`App listening on port ${port}`);
-  });
-  // [END server]
+let uri = `mongodb://${user}:${pass}@${host}:${port}`;
+if (nconf.get('mongoDatabase')) {
+    uri = `${uri}/${nconf.get('mongoDatabase')}`;
 }
+console.log(uri);
+
+mongodb.MongoClient.connect(uri, function(err, db) {
+    // Attempt to connect to the MongoDB server on mLab
+    if (err) {
+        throw err;
+    } else {
+        console.log("connected");
+    }
+
+// Sort and print all documents from tshirts collection to console
+var dbo = db.db('items');   // database is called items in mLab
+var mysort = { itemid: 1 }; // sort by itemid in ascending order
+
+dbo.collection("tshirts").find().sort(mysort).toArray(function(err, result) {
+    if (err) {
+        throw err;
+    }
+    console.log(result);
+    db.close();
+});
+
+// Previous testing code to print all items in tshirt collection
+/*
+dbo.collection("tshirts").find({}).toArray(function(err, result) {
+    if (err) {
+        throw err;
+    }
+    console.log(result);
+    db.close();
+});
+*/
+    // Load the static html/css/js files
+    app.get('/', function(req, res){
+        res.sendFile('index.html', { root: __dirname + "/public/html" } );
+    });
+    app.use(express.static("public"));
+
+    // Start the server with Express.js
+    if (module === require.main) {
+        // [START server]
+        const server = app.listen(process.env.PORT || 8080, () => {
+        const port = server.address().port;
+            console.log(`App listening on port ${port}`);
+        });
+        // [END server]
+    }
+});
 
 module.exports = app;
 
