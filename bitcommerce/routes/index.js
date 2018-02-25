3e997e5e247965c1ce82a049a69444f9a41e92b1
Chris Ta <CTaCDE@users.noreply.github.com>
2018-02-24 18:06:27 -0800

    Convert header of index.html into pug

--- bitcommerce/routes/index.js
+++ bitcommerce/routes/index.js
@@ -3,8 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  //res.render('index', { title: 'Express' });
-  res.redirect('/catalog');
+  res.render('index', { title: '193Tees' });
+  //res.redirect('/catalog');
 });
 
 module.exports = router;
