fae57bf4cb3c3c1e1b9291cc18e1ff708ce30d9a
phuonghn <hptnguyen@ucdavis.edu>
2018-04-14 18:35:20 -0700

    Fix merge conflict in app.js

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ var confirmation = require('./routes/confirmation');
 var logs = require('./routes/logs');
 var terms = require('./routes/terms');
 var privacypolicy = require('./routes/privacypolicy');
+var myaccount = require('./routes/myaccount');
 
 var app = express();
 
@@ -62,6 +63,7 @@ app.use('/confirmation', confirmation);
 app.use('/logs',logs);
 app.use('/terms',terms);
 app.use('/privacypolicy', privacypolicy);
+app.use('/myaccount', myaccount);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
