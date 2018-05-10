c669d9d04f0cb7a4211ee14f97cf239b7b7a3d48
Chris Ta <CTaCDE@users.noreply.github.com>
2018-05-09 17:51:56 -0700

    Fix http typo (all urls should be secure)

--- controllers/userscontroller.js
+++ controllers/userscontroller.js
@@ -380,7 +380,7 @@ exports.postForgot = (req, res, next) => {
       subject: 'Reset your password on Hackathon Starter',
       text: `You are receiving this email because you (or someone else) have requested the reset of the password for your account.\n\n
         Please click on the following link, or paste this into your browser to complete the process:\n\n
-        http://${req.headers.host}/reset/${token}\n\n
+        https://${req.headers.host}/reset/${token}\n\n
         If you did not request this, please ignore this email and your password will remain unchanged.\n`
     };
     return transporter.sendMail(mailOptions)
