94b3bb029bfa7c5202a5ba398c66571c1d225f26
r-eter <reter@ucdavis.edu>
2018-05-20 19:10:52 -0700

    google analytics version check integration

--- app.js
+++ app.js
@@ -221,5 +221,55 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+/**
+ * Google Analytics - Version Checking integration.
+ */
+'use strict';
+
+app.enable('trust proxy');
+
+// The following environment variable is set by app.yaml when running on App
+// Engine, but will need to be set manually when running locally. See README.md.
+const GA_TRACKING_ID = process.env.GA_TRACKING_ID;
+
+function trackEvent (category, action, label, value, cb) {
+  const data = {
+    // API Version.
+    v: '2',
+    // Tracking ID / Property ID.
+    tid: GA_TRACKING_ID,
+    // Anonymous Client Identifier. Ideally, this should be a UUID that
+    // is associated with particular user, device, or browser instance.
+    cid: '555',
+    // Event hit type.
+    t: 'event',
+    // Event category.
+    ec: category,
+    // Event action.
+    ea: action,
+    // Event label.
+    el: label,
+    // Event value.
+    ev: value
+  };
+
+  return got.post('http://www.google-analytics.com/collect', {
+    form: data
+  });
+}
+
+app.get('/', (req, res, next) => {
+  // Event value must be numeric.
+  trackEvent('Example category', 'Example action', 'Example label', '100')
+    .then(() => {
+      res.status(200).send('Event tracked.').end();
+    })
+    // This sample treats an event tracking error as a fatal error. Depending
+    // on your application's needs, failing to track an event may not be
+    // considered an error.
+    .catch(next);
+});
+
+
 module.exports = app;
 
